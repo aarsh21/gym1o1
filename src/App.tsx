5a69a7f617feb9a017566bd1c6181388751af3f6
aarsh21 <aarshpersonal21@gmail.com>
2023-05-22 18:27:58 +0530

    INITIALIZED VITE AND CREATED NAV

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import Navbar from "@/scnens/navbar";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "./shared/types";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setisTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setisTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setisTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <>
+      <div className="app bg-gray-20">
+        <Navbar 
+        isTopOfPage ={isTopOfPage}
+        selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      </div>
+    </>
+  );
+}
+
+export default App;
